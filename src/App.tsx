acd8fb49727aee0875d9db2b38249aca50919a24
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-08-09 23:15:01 +0530

    create folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductPage from "./ProductPage";
 import Discounts from "./Discounts";
-import LeadCapture from './LeadCapture'
+import LeadCapture from "./LeadCapture";
+import Poststay from "./Poststay";
 
 export default function App() {
   return (
@@ -10,7 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/product" element={<ProductPage />} />
         <Route path="/room" element={<Discounts />} />
-        <Route path="/modal" element={<LeadCapture/>}/>
+        <Route path="/modal" element={<LeadCapture />} />
+        <Route path="/poststay" element={<Poststay />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Poststay.tsx
@@ -0,0 +1,11 @@
+import React, { useState, Component } from "react";
+
+const Poststay: React.FC = () => {
+  return (
+    <div className="App font mb-24 text-black mt-16">
+      <div className="max-w-lg mx-auto py-5 px-4 sm:py-5u sm:px-0 lg:max-w-xl lg:px-8 "></div>
+    </div>
+  );
+};
+
+export default Poststay;
