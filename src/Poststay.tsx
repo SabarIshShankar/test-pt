cacc7eac25eaa9ccd67c090ee09a1d235c842021
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-08-09 19:34:37 +0000

    new

--- src/Poststay.tsx
+++ src/Poststay.tsx
@@ -1,9 +1,12 @@
 import React, { useState, Component } from "react";
+import Header from './prototype/Header'
 
 const Poststay: React.FC = () => {
   return (
     <div className="App font mb-24 text-black mt-16">
-      <div className="max-w-lg mx-auto py-5 px-4 sm:py-5u sm:px-0 lg:max-w-xl lg:px-8 "></div>
+      <div className="max-w-lg mx-auto py-5 px-3 sm:py-5u sm:px-0 lg:max-w-xl lg:px-8">
+        <Header/>
+      </div>
     </div>
   );
 };
